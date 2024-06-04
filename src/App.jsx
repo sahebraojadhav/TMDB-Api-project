79b44cff4ef9eb163cb0e860dab2c95a455e7650
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-04 07:45:07 +0530

    intialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     hellow guys
+    </>
+  )
+}
+
+export default App
