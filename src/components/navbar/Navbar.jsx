60dce22f67075aeb8a241ad1c26f89ecdfef75b8
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-09 10:17:40 +0530

    created autocomplete bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,47 @@
-import React from "react";
-import AbcIcon from '@mui/icons-material/Abc';
-import ModeNightIcon from '@mui/icons-material/ModeNight';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
+import React, { useRef } from "react";
+import AbcIcon from "@mui/icons-material/Abc";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
 
 import "./navbar.css";
 function Navbar() {
-
- 
+  const resultListRef = useRef(null);
 
   return (
     <div className="nav-div">
       <div className="live-tv">
-      <LiveTvIcon style={{fontSize:'40px',color:"white"}} variant="large"/>
-       Movies
+        <LiveTvIcon
+          style={{ fontSize: "40px", color: "white" }}
+          variant="large"
+        />
+        Movies
       </div>
       <div className="navbar">
-        <input type="text" className="input-field" placeholder="search...." />
+        <input
+          id="movie-search-input"
+          type="text"
+          onFocus={()=>
+            {resultListRef.current.style.display='block'}
+          }
+          onBlur={()=>{
+            resultListRef.current.style.display='none'
+          }}
+          className="input-field"
+          placeholder="search...."
+        />
         <button className="search-btn">Search</button>
+        <div className="result-list" ref={resultListRef}>
+            <div className="result">result 1</div>
+            <div className="result">result 1</div>
+            <div className="result">result 1</div>
+          
+        </div>
       </div>
       <div className="night-mode">
-      <ModeNightIcon style={{fontSize:'50px',color:"white"}} className="navbar-icon"/> 
+        <ModeNightIcon
+          style={{ fontSize: "50px", color: "white" }}
+          className="navbar-icon"
+        />
       </div>
     </div>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,47 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import MovieCard from '../MovieCard/MovieCard'
-import Navbar from '../navbar/Navbar'
-import './Home.css'
-import axios from 'axios'
-import { Movie } from '@mui/icons-material'
+import React, { useEffect, useState } from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import Navbar from "../navbar/Navbar";
+import "./Home.css";
+import axios from "axios";
+import { Movie } from "@mui/icons-material";
+import StarIcon from '@mui/icons-material/Star';
 
-import {searchMovie} from '../apis/omdb'
+import { searchMovie } from "../apis/omdb";
 
 function Home() {
-  const[MovieList,setMovieList]=useState([]);
+  const [MovieList, setMovieList] = useState([]);
 
-  async function downloadDefaultMovie(...args){
-    const urls=args.map((name)=> searchMovie(name));
+  async function downloadDefaultMovie(...args) {
+    const urls = args.map((name) => searchMovie(name));
     console.log(urls);
-    const response=await axios.all(urls.map(url=>axios.get(url)));
+    const response = await axios.all(urls.map((url) => axios.get(url)));
     console.log(response);
-    const movies=response.map((movieResponse)=>movieResponse.data.Search)
+    const movies = response.map((movieResponse) => movieResponse.data.Search);
     setMovieList([].concat(...movies));
   }
 
-  useEffect(()=>{
-    downloadDefaultMovie('harry','avengers','batman');
-  },[])
-  
+  useEffect(() => {
+    downloadDefaultMovie("harry", "avengers", "batman");
+  }, []);
 
   return (
-    <div>
+    <div className="main-home-div">
       {/*navbar*/}
       {/*MovieList*/}
       {/*pagination button*/}
-      <Navbar/>
+      <Navbar />
       <div className="movie-card-div">
-     {
-       MovieList.map((movie,idx)=>(<MovieCard key={idx} Title={movie.Title} Year={movie.Year} Type={movie.Type} Poster={movie.Poster}/>))
-     }
-
-      
-
+        {MovieList.map((movie, idx) => (
+          <MovieCard
+            key={idx}
+            Title={movie.Title}
+            Year={movie.Year}
+            Type={movie.Type}
+            Poster={movie.Poster}
+          />
+        ))}
       </div>
-
-      
+      <div className="rating-card">
+        <h4>Give us rating</h4>
+        <StarIcon/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
